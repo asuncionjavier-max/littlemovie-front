b8e7933174792deac95bcae3885d042fcd9e0b34
Javi <asuncionjavier96@gmail.com>
2026-09-13 17:39:40 +0200

    refactor: add rating

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,17 +1,18 @@
-import styles from "./MovieCard.module.css"
+import styles from "./MovieCard.module.css";
 function MovieCard({ movie }) {
-
-    if(!movie) return null
-    return(
-    
+  if (!movie) return null;
+  return (
     <article className={styles.card}>
-        <img src={movie.movie_image} alt="" />
-        <h2><span >{movie.title}</span></h2>
-        <h3> {movie.director}</h3>
-        <h3>{movie.year}</h3>
-        <p>{movie.price}</p>
+      <img src={movie.movie_image} alt="" />
+      <h2>
+        <span>{movie.title}</span>
+      </h2>
+      <h3> Director: {movie.director}</h3>
+      <h3>Año : {movie.year}</h3>
+      <p>Rating: {movie.rating}</p>
+      <p> Precio: {movie.price}</p>
     </article>
-)
+  );
 }
 
 export default MovieCard;
