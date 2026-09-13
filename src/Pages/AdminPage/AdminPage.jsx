c6abf2c4744eb8a40033c025fe5b61ea67edb160
Javi <asuncionjavier96@gmail.com>
2026-09-13 16:09:37 +0200

    fix: AdminPage preview img, refactor: style code AdminPage & header

--- src/Pages/AdminPage/AdminPage.jsx
+++ src/Pages/AdminPage/AdminPage.jsx
@@ -1,107 +1,125 @@
 import apiClient from "../../config/axios";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import styles from "./AdminPage.module.css";
-import { append } from "dom/lib/mutation";
 
 function AdminPage() {
-    const titleRef = useRef();
-    const yearRef = useRef();
-    const directorRef = useRef();
-    const genreRef = useRef();
-    const priceRef = useRef();
-    const ratingRef = useRef();
-    const imagenRef = useRef();
+  const [selectedFile, setSelectedFile] = useState(null);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const titleRef = useRef();
+  const yearRef = useRef();
+  const directorRef = useRef();
+  const genreRef = useRef();
+  const priceRef = useRef();
+  const ratingRef = useRef();
+  const imagenRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
     const formData = new FormData();
 
-        formData.append("title", titleRef.current.value),
-        formData.append ("year", yearRef.current.value),
-        formData.append ("director", directorRef.current.value),
-        formData.append("genre", genreRef.current.value),
-        formData.append("price", priceRef.current.value),
-        formData.append("rating", ratingRef.current.value)
+    (formData.append("title", titleRef.current.value),
+      formData.append("year", yearRef.current.value),
+      formData.append("director", directorRef.current.value),
+      formData.append("genre", genreRef.current.value),
+      formData.append("price", priceRef.current.value),
+      formData.append("rating", ratingRef.current.value));
 
-        if(imagenRef.current.files[0]){
-            formData.append("image", imagenRef.current.files[0]);
-        } 
+    if (imagenRef.current.files[0]) {
+      formData.append("image", imagenRef.current.files[0]);
+    }
 
     try {
-        await apiClient.post("/movies", formData,);
-            alert("Película publicada con éxito 🎬");
-        e.target.reset();
+      await apiClient.post("/movies", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      alert("Película publicada con éxito 🎬");
+      e.target.reset();
+      setSelectedFile(null);
     } catch (error) {
-        console.error("Error al publicar la película:", error);
-        alert("Hubo un error al guardar la película.");
+      console.error("Error al publicar la película:", error);
+      alert("Hubo un error al guardar la película.");
     }
-};
+  };
 
-return (
+  return (
     <main className={styles.container}>
-        <header className={styles.header}>
-            <h1>Panel de Administración</h1>
-            <p>Publica y gestiona las películas del catálogo</p>
-        </header>
+      <header className={styles.header}>
+        <h1>Panel de Administración</h1>
+        <p>Publica y gestiona las películas del catálogo</p>
+      </header>
 
-    <div className={styles.card}>
+      <div className={styles.card}>
         <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.formGroup}>
+          <div className={styles.formGroup}>
             <label htmlFor="movieTitle">Título de la película</label>
             <input type="text" id="movieTitle" required ref={titleRef} />
-        </div>
+          </div>
 
-        <div className={styles.row}>
+          <div className={styles.row}>
             <div className={styles.formGroup}>
-                <label htmlFor="yearMovie">Año</label>
-                <input type="number" id="yearMovie" ref={yearRef}  />
+              <label htmlFor="yearMovie">Año</label>
+              <input type="number" id="yearMovie" ref={yearRef} />
             </div>
 
             <div className={styles.formGroup}>
-                <label htmlFor="price">Precio (€)</label>
-                <input type="number" step="0.01" id="price" required ref={priceRef} />
+              <label htmlFor="price">Precio (€)</label>
+              <input
+                type="number"
+                step="0.01"
+                id="price"
+                required
+                ref={priceRef}
+              />
             </div>
-        </div>
+          </div>
 
-        <div className={styles.formGroup}>
+          <div className={styles.formGroup}>
             <label htmlFor="movieDirector">Director</label>
-            <input type="text" id="movieDirector" required ref={directorRef}/>
-        </div>
+            <input type="text" id="movieDirector" required ref={directorRef} />
+          </div>
 
-        <div className={styles.row}>
+          <div className={styles.row}>
             <div className={styles.formGroup}>
-                <label htmlFor="genre">Género</label>
-                <input type="text" id="genre" required ref={genreRef} />
+              <label htmlFor="genre">Género</label>
+              <input type="text" id="genre" required ref={genreRef} />
             </div>
 
             <div className={styles.formGroup}>
-                <label htmlFor="rating">Rating</label>
-                <input type="number" step="0.1" id="rating" required ref={ratingRef} />
+              <label htmlFor="rating">Rating</label>
+              <input
+                type="number"
+                step="0.1"
+                id="rating"
+                required
+                ref={ratingRef}
+              />
             </div>
-            
-        </div>
-        <div className={styles.fileGroup}>
+          </div>
+          <div className={styles.fileGroup}>
             <label htmlFor="movieImage">Portada (Imagen)</label>
-            <input 
-            type="file" 
-            id="movieImage" 
-            accept="image/*" 
-            ref={imagenRef} 
-            className={styles.fileInput}
-        />
+            <input
+              type="file"
+              id="movieImage"
+              accept="image/*"
+              ref={imagenRef}
+              className={styles.fileInput}
+              onChange={(e) => setSelectedFile(e.target.files[0])}
+            />
             <label htmlFor="movieImage" className={styles.fileCustomLabel}>
-                <span>📁</span> Seleccionar portada de película
+              <span>📁</span>{" "}
+              {selectedFile
+                ? selectedFile.name
+                : "Seleccionar portada de película"}
             </label>
-        </div>
-        <button type="submit"
-        className={styles.submitBtn}>
+          </div>
+          <button type="submit" className={styles.submitBtn}>
             Publicar película
-        </button>
+          </button>
         </form>
-    </div>
+      </div>
     </main>
-);
+  );
 }
 
 export default AdminPage;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,60 +3,63 @@ import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { clearUser, selectIsAdmin } from "../../store/slices/authSlices";
 import apiCLient from "../../config/axios";
+import toast from "react-hot-toast";
 
 function Header() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const user = useSelector((state) => state.auth.user);
-    const isAdmin = useSelector(selectIsAdmin)
-    const cartItems = useSelector((state) => state.cart.items);
+  const user = useSelector((state) => state.auth.user);
+  const isAdmin = useSelector(selectIsAdmin);
+  const cartItems = useSelector((state) => state.cart.items);
 
-
-    const handleOut = async () =>{
-        try {
-            await apiCLient.post("/logout");
-        } catch (error) {
-            console.error("Error al cerrar sesion", error);
-        } finally{
-            dispatch(clearUser());   
-            navigate("/login");
-        }
-    };
-return (
+  const handleOut = async () => {
+    try {
+      const toastId = toast.loading(`Cerrando session`);
+      await apiCLient.post("/logout");
+      toast.success("Session cerrada con exito", { id: toastId });
+    } catch (error) {
+      toast.error("Error al cerrar sesion", error);
+      console.error("Error al cerrar sesion", error);
+    } finally {
+      dispatch(clearUser());
+      navigate("/login");
+    }
+  };
+  return (
     <header className={styles.header}>
-    <Link to="/" className={styles.logo}>
+      <Link to="/" className={styles.logo}>
         Little <span>Movie</span>
-    </Link>
+      </Link>
 
-    <nav className={styles.nav}>
+      <nav className={styles.nav}>
         <Link to="/">Inicio</Link>
         <Link to="/movies">Películas</Link>
         <Link to="/about">Sobre nosotros</Link>
         {isAdmin && <Link to="/admin">Administrador</Link>}
-    </nav>
+      </nav>
 
-        <div className={styles.actions}>
+      <div className={styles.actions}>
         {user ? (
-        <div className={styles.userControls}>
+          <div className={styles.userControls}>
             <Link to="/cart" title="Carrito">
-            🛒
+              🛒
             </Link>
-            <Link to = "/profile">
-            <span className={styles.userName}>👤 {user.name}</span>
+            <Link to="/profile">
+              <span className={styles.userName}>👤 {user.name}</span>
             </Link>
             <button onClick={handleOut} className={styles.logoutBtn}>
-            Logout
+              Logout
             </button>
-        </div>
+          </div>
         ) : (
-        <Link to="/login" className={styles.loginBtn}>
+          <Link to="/login" className={styles.loginBtn}>
             Login
-        </Link>
+          </Link>
         )}
-    </div>
+      </div>
     </header>
-    );
+  );
 }
 
 export default Header;
